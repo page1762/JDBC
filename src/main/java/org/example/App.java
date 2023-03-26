7dccecaa1f4e87c87537621dd5df0f6a9cbf0a70
page1762 <page1762@gmail.com>
2023-03-26 19:49:53 +0900

    게시물 리스팅, 추가 기능 구현

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,22 +1,52 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
   public void run() {
     Scanner sc = Container.scanner;
 
-    while(true){
-      System.out.printf("명령어) ");
-      String cmd  = sc.nextLine();
+    List<Article> articles = new ArrayList<>();
+    int articleLastId = 0;
 
-      if(cmd.equals("article list")){
+    while (true) {
+      System.out.printf("명령어) ");
+      String cmd = sc.nextLine();
+
+      if (cmd.equals("/usr/article/write")) {
+        System.out.println("==게시물 등록==");
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+        System.out.printf("내용 : ");
+        String body = sc.nextLine();
+        int id = articleLastId + 1;
+        articleLastId++;
+
+        Article article = new Article(id, title, body);
+        articles.add(article);
+
+        System.out.println("생성된 게시물 객체 : " + article);
+        System.out.printf("%d번 게시물이 입력되었습니다.\n", article.id);
+      }
+      else if (cmd.equals("/usr/article/list")) {
         System.out.println("==게시물 리스트==");
-      } else if(cmd.equals("system exit")){
+
+        if(articles.isEmpty()){
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+        System.out.println("번호 / 제목");
+
+        for(Article article : articles){
+          System.out.printf("%d / %s\n",article.id, article.title);
+        }
+      }
+      else if (cmd.equals("system exit")) {
         System.out.println("시스템 종료");
         break;
-      }
-      else{
+      } else {
         System.out.println("명령어를 확인해주세요.");
       }
     }
